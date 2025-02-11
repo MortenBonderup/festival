3cb0da10a904802c9e3110651d414ace6f30fb55
Morten Bonderup <moab@eaaa.dk>
2025-02-11 09:13:11 +0100

    Version after search and category

--- src/views/StagePage.jsx
+++ src/views/StagePage.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 export default function StagePage() {
 
     const stageevents = [
-        { stageid: "1", eventtext: "Coldplay", uid: "1" },
-        { stageid: "1", eventtext: "Queen", uid: "2" },
-        { stageid: "2", eventtext: "Billie Elish", uid: "3" },
-        { stageid: "2", eventtext: "Lady Gaga", uid: "4" }
+        { stageid: "1", eventtext: "Coldplay", uid: "1", category: "Pop" },
+        { stageid: "1", eventtext: "Queen", uid: "2", category: "Rock" },
+        { stageid: "1", eventtext: "Van Morrison", uid: "3", category: "Folk" },
+        { stageid: "1", eventtext: "Toto", uid: "4", category: "Pop" },
+        { stageid: "2", eventtext: "Billie Elish", uid: "5", category: "Pop" },
+        { stageid: "2", eventtext: "Lady Gaga", uid: "4", category: "Pop" }
     ];
 
     const { stageId } = useParams(); // Get stageId from link
+    const [searchTerm, setSearchTerm] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     // Get events that matches stageid and id from link
     // Result: an array of stageevents called "events"
     const events = stageevents.filter((event,) => stageId === event.stageid);
+
+    const filteredEvents = events.filter(event => {
+        const matchesSearch = event.eventtext.toLowerCase().includes(searchTerm.toLowerCase());
+        const matchesCategory = selectedCategory === 'all' || event.category === selectedCategory;
+        return matchesSearch && matchesCategory;
+    })
+
+
     return (
         <div>
+            Search by artist:
+            <input type="text" id="search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for artist" />
+
+            Filter by category:
+            <select id="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                <option value="all">All</option>
+                <option value="Pop">Pop</option>
+                <option value="Rock">Rock</option>
+                <option value="Folk">Folk</option>
+            </select>
             <h2>List of events on stage {stageId}</h2>
-            {events.map((event) => (
+            {filteredEvents.map((event) => (
                 <div key={event.id}>
                     <Link to={`/festival/detailpage/${event.uid}`}>{event.eventtext}</Link>
                 </div>
